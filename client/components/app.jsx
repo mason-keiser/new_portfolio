17bcafced71b6000965f18a929f9d642da02f0fe
Mason Keiser <masonksr5@gmail.com>
2021-01-21 04:50:30 +0000

    basic file construction

---
+++ client/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor() {
+      super();
+    }
+  
+    render() {
+      return (
+        <div>
+            welcome to app
+        </div>
+      );
+    }
+  }
+  export default App;
+  
