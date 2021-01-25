75e59699fbd28f99012d0ff3dc1333b9d70acda2
Mason Keiser <masonksr5@gmail.com>
2021-01-25 20:20:05 +0000

    can see github graph on about comp

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 export default class About extends React.Component {
     constructor(props) {
         super(props)
+        this.loadCalendar = this.loadCalendar.bind(this)
+    }
+
+    componentDidMount() {
+        this.loadCalendar()
+    }
+
+    loadCalendar() {
+        GitHubCalendar(".calendar", "mason-keiser", {
+            summary_text: null,
+            responsive: true
+        })
     }
 
 
@@ -10,7 +22,7 @@ export default class About extends React.Component {
         return(
             <div className='cont' id='about'>
                 <div className='aboutCont'>
-
+                    <div className='calendar'></div>
                 </div>
             </div>
         )
