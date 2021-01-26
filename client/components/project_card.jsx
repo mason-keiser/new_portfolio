cb6552e9cb714782cd8c4182f28a013e7020666f
Mason Keiser <masonksr5@gmail.com>
2021-01-26 21:21:04 +0000

    more styling done to proj card

--- client/components/project_card.jsx
+++ client/components/project_card.jsx
@@ -9,7 +9,7 @@ export default function ProjectCard(props) {
  
   
   return (
-    <Card id='projCard' className="h-100 rounded border slide-in-10 mt-3 mb-3"
+    <Card id='projCard' className="h-100 rounded border slide-in-10 mt-5 mb-5"
       style={{ animationDelay: props.delay + 'ms' }}>
       <CardBody
         className="p-0 h-25">
@@ -23,11 +23,12 @@ export default function ProjectCard(props) {
             alt={props.project.alt}
             id='proj-Img' />
         </a>
-        <div className='p-3'>
-            <h5 className="text-center mt-3 font-weight-bold">
+        <div className='p-3 '>
+            <h5 className=" mt-3 font-weight-bold">
                 {props.project.name}
             </h5>
-            <div className="d-flex align-content-start flex-wrap w-100 border-dark">
+            <h6 id='sub' className='mt-3 mb-2'>{props.project.subname}</h6>
+            <div className="d-flex align-content-start flex-wrap w-100 border-dark mt-3">
                 {(props.project.description)}
             </div>
         </div>
