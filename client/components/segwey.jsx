99297e10ff660e1a56664fd315debf9ba85f94ee
Mason Keiser <masonksr5@gmail.com>
2021-01-27 22:53:28 +0000

    everything is on the page

--- client/components/segwey.jsx
+++ client/components/segwey.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import StarfieldAnimation from 'react-starfield-animation';
+import {
+    Container,
+    Row,
+    Col
+  } from 'reactstrap';
+import Segwey_Card from './segwey_card'
 
 export default class Segwey extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            descriptions: [
+                {
+                    adj: 'Hard Worker'
+                },
+                {
+                    adj: 'Motivated'
+                },
+                {
+                    adj: 'Dependable'
+                },
+                {
+                    adj: 'Team Player'
+                }
+            ]
+        }
     }
 
     render() {
+        let delay  = 0;
         return (
             <div className='segweyCont'>
                  <StarfieldAnimation className='starfield'
@@ -17,6 +40,31 @@ export default class Segwey extends React.Component {
                 height: 'inherit',
                 margin: '0'
                 }}/>
+                <div className="segwey-section mb-4 fade-in" id="projects" style={{height: '100%'}}>
+                    <Container className='d-flex justify-content-center align-content-center align-items-center' style={{height: '100%'}}>
+                        <Row>
+                            <Col xs="12">
+                                <Row className=" fade-in row-cols-1 row-cols-md-4 row-cols-lg-4 justify-content-center pt-5">
+                                    {
+                                        this.state.descriptions.map((desc, index) => {
+                                            delay += 100;
+                                            return (
+                                              <Col key={index}
+                                                className="mb-2" id='fold'>
+                                                {desc.adj
+                                                  ? <Segwey_Card desc={desc}
+                                                    delay={delay}
+                                                    />
+                                                  : null}
+                                              </Col>
+                                            );
+                                          })
+                                    }
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
         )
     }
---
+++ client/components/segwey_card.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Col,
+  Card,
+  CardBody
+} from 'reactstrap';
+import LazyLoad from 'react-lazy-load';
+
+
+
+
+export default class Segwey_Card extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+  return (
+    <Col 
+      className="mr-5 ml-5 mobile-card m-auto p-0"  style={{background:'transparent'}}>
+      <Card data-tilt className=' js-tilt h-100 skills-card border-0 slide-in-10 p-0'  style={{background:'transparent'}}>
+        <CardBody className='m-auto' style={{flex: 'none'}} id='inner'>
+        <div className='d-flex flex-column justify-content-center'>
+          <LazyLoad
+            className='m-auto'
+            debounce={false}
+            height={96}
+            offsetVertical={350}>
+            <div className="decoration-none border-div-small d-flex" id='icon3'>
+                <i id='skillIcon' alt="Icon"
+                className='fas fa-check'/>
+            </div>
+          </LazyLoad>
+          <div className="my-3">
+            <h4 className="text-center" style={{color: 'white'}}>{this.props.desc.adj}</h4>
+          </div>
+        </div>
+        </CardBody>
+      </Card>
+    </Col>
+    )
+  }
+}
